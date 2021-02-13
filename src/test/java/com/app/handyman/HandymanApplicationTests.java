0b7f409c8538b285c3a41db1a35d518949e60571
warthex <yonathanw@outlook.es>
2021-02-13 00:03:39 -0500

    end with test :D

--- src/test/java/com/app/handyman/HandymanApplicationTests.java
+++ src/test/java/com/app/handyman/HandymanApplicationTests.java
@@ -4,24 +4,26 @@ import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.app.handyman.dto.HoursReportDto;
 import com.app.handyman.dto.ResponseDto;
 import com.app.handyman.entity.ServiceReportEntity;
 import com.app.handyman.service.IServiceReportService;
 
 import reactor.core.publisher.Mono;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class HandymanApplicationTests {
 
@@ -52,31 +54,36 @@ class HandymanApplicationTests {
 
 		Mockito.when(repository.addServiceReport(report)).thenReturn(Mono.just(response));
 
-		webClient.post().uri("add").contentType(MediaType.APPLICATION_JSON)
-				.body(Mono.just(response), ResponseDto.class).exchange().expectStatus().isOk();
+		webClient.post().uri("add").contentType(MediaType.APPLICATION_JSON).body(Mono.just(response), ResponseDto.class)
+				.exchange().expectStatus().isOk();
 
 		Mockito.verify(repository, times(0)).addServiceReport(report);
 	}
+
 	@Test
-	void testGetReport() {
-		ServiceReportEntity report = new ServiceReportEntity();
-		ResponseDto response = new ResponseDto(true, "ok");
-		
-		String technicalId ="wtx-1";
-		String weekNumber ="4";
-		
-		report.setTechnicalId("wtx-1");
-		report.setServiceId("s-99");
-		report.setStartDate("2021-01-18T07:00");
-		report.setEndDate("2021-01-20T07:15");
-		report.setWeekNumber("4");
+	void testGetReport() throws InterruptedException {
+		HoursReportDto report = new HoursReportDto();
 
-		Mockito.when(repository.addServiceReport(report)).thenReturn(Mono.just(response));
+		String technicalId = "wtx-1";
+		String weekNumber = "4";
 
-		webClient.get().uri("add").contentType(MediaType.APPLICATION_JSON)
-				.body(Mono.just(response), ResponseDto.class).exchange().expectStatus().isOk();
+		report.setNormalHours(1.3);
+		report.setNormalExtraHours(3.4);
 
-		Mockito.verify(repository, times(0)).addServiceReport(report);
+		report.setNightHours(4.1);
+		report.setNightlyOvertime(3.1);
+
+		report.setSundayHours(0.0);
+		report.setSundayExtraHours(7.0);
+
+		Mockito.when(repository.calculateHours(technicalId, weekNumber)).thenReturn(Mono.just(report));
+
+		webClient.get()
+				.uri(uriBuilder -> uriBuilder.path("report").queryParam("technicalId", technicalId)
+						.queryParam("weekNumber", weekNumber).build())
+				.exchange().expectStatus().isOk().expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+				.expectBody(HoursReportDto.class).isEqualTo(report);
+
 	}
 
 }
